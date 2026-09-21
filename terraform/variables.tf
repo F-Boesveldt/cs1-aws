fbca9a524cb696aea91486a9e0d6dcc20b0f797b
F-Boesveldt <f.boesveldt@student.fontys.nl>
2026-09-21 11:05:01 +0200

    Change default region from eu-north-1 to eu-central-1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region for all resources"
   type        = string
-  default     = "eu-north-1"
+  default     = "eu-central-1"
 }
 
 variable "project_name" {
