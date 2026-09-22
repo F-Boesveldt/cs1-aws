b8e9dfffb9d3c14dfe3abedf237344732e96e812
Floris Boesveldt <f.boesveldt@student.fontys.nl>
2026-09-22 10:08:40 +0200

    Run terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,24 +8,24 @@ module "network" {
 module "compute" {
   source = "./modules/compute"
 
-  region                = var.region
-  project_name          = var.project_name
-  vpc_id                = module.network.vpc_id
-  web_subnet_id         = module.network.web_subnet_id
-  web_sg_id             = module.network.web_sg_id
-  db_sg_id              = module.network.db_sg_id
-  db_subnet_ids         = module.network.db_subnet_ids
-  public_subnet_ids     = module.network.public_subnet_ids
-  admin_ssh_public_key  = var.admin_ssh_public_key
-  db_admin_password     = var.db_admin_password
+  region               = var.region
+  project_name         = var.project_name
+  vpc_id               = module.network.vpc_id
+  web_subnet_id        = module.network.web_subnet_id
+  web_sg_id            = module.network.web_sg_id
+  db_sg_id             = module.network.db_sg_id
+  db_subnet_ids        = module.network.db_subnet_ids
+  public_subnet_ids    = module.network.public_subnet_ids
+  admin_ssh_public_key = var.admin_ssh_public_key
+  db_admin_password    = var.db_admin_password
 }
 
 module "monitoring" {
   source = "./modules/monitoring"
 
-  region                = var.region
-  project_name          = var.project_name
-  monitoring_subnet_id  = module.network.monitoring_subnet_id
-  monitoring_sg_id      = module.network.monitoring_sg_id
-  key_name              = module.compute.key_name
+  region               = var.region
+  project_name         = var.project_name
+  monitoring_subnet_id = module.network.monitoring_subnet_id
+  monitoring_sg_id     = module.network.monitoring_sg_id
+  key_name             = module.compute.key_name
 }
