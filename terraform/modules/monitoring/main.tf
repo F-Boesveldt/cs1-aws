ae92f5ee312894f82ee4f0220c5b552709127b1a
F-Boesveldt <f.boesveldt@student.fontys.nl>
2026-09-23 09:31:18 +0200

    Update Ubuntu AMI data source in monitoring module
    Replaced the aws_ami data source with aws_ssm_parameter for Ubuntu AMI.

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -5,14 +5,8 @@ data "aws_iam_instance_profile" "lab" {
   name = "LabInstanceProfile"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+data "aws_ssm_parameter" "ubuntu_ami" {
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
 
 resource "aws_instance" "monitoring" {
