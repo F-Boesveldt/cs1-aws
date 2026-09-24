3c5885b4b4b11a408b9948f953e99bf26bc108ee
F-Boesveldt <f.boesveldt@student.fontys.nl>
2026-09-24 09:36:24 +0200

    Remove unused aws_ssm_parameter data source
    Removed unused data source for Ubuntu AMI.

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -5,10 +5,6 @@ data "aws_iam_instance_profile" "lab" {
   name = "LabInstanceProfile"
 }
 
-data "aws_ssm_parameter" "ubuntu_ami" {
-  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
-}
-
 resource "aws_instance" "monitoring" {
   ami                    = "ami-0303e2e4a29f041a3"
   instance_type          = "t3.medium"
